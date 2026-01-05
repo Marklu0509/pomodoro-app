54f3920f6feb4a060b9b6a9e60d9682e6122d216
chlu0889 <chlu0889@uni.sydney.edu.au>
2026-01-05 17:58:22 +0800

    Feat: Add Light/Dark/System theme selector in Settings2

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -3,6 +3,7 @@
 
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import api from "../../utils/api";
 
 export default function Navbar() {
   const router = useRouter();
